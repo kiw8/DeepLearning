e6f87ddbf9471f52aa5ba23e6747fe5ec4e5f1d1
jeongwoo <dolmenari4@naver.com>
2024-12-02 14:43:36 +0900

    neural network 학습

---
+++ jeongwoo 2.py
@@ -0,0 +1,22 @@
+inputs = [1.0, 2.0 , 3.0 , 0.6]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+import random
+
+def init_weights(inputs):
+    return [random.uniform(-1,1) for i in inputs]
+
+def cal(inputs, weights, bias):
+    output = []
+    return output
+
+
+def cal_neuron(num_neuron,inputs):
+    output = []
+    for i in range(num_neuron):
+        weights = init_weights(len(inputs))
+        bias = random.uniform(-1,1)
+        output = cal(inputs, weights , bias)
+        output.append(output)
+    return output
